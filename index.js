fddf6d14b25581cb4dc069784220d7f68c85969f
sabbirsun <sabbirsun1920@gmail.com>
2024-01-14 18:12:19 +0600

    static method

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ console.log(BookDetails);*/
 
 // Class Expression
 
-let person = class {
+/*let person = class {
     constructor(name,age,profe){
         this.name = name;
         this.age = age;
@@ -21,4 +21,13 @@ let person = class {
 }
 
 let newPerson = new person("Sabbir Sun" , "23" , "Programmer");
-console.log(newPerson)
+console.log(newPerson);*/
+
+// Static Method
+
+class Book{
+    static fun() {// class name diye static er vitore function call kora
+        console.log("Sabbir Sun");
+    }
+}
+Book.fun()

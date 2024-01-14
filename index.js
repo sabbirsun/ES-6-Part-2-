43dec0b8a6013c965a5245798167ab2237737b72
sabbirsun <sabbirsun1920@gmail.com>
2024-01-14 17:55:51 +0600

    es6 part 2

---
+++ index.js
@@ -0,0 +1,24 @@
+// Class Constructor
+
+/*class Book{
+    constructor(BookName, Price){
+        this.BookName = BookName;
+        this.Price = Price;
+    }
+}
+
+let BookDetails = new Book("Opekkha", "500 Tk");
+console.log(BookDetails);*/
+
+// Class Expression
+
+let person = class {
+    constructor(name,age,profe){
+        this.name = name;
+        this.age = age;
+        this.profe = profe;
+    }
+}
+
+let newPerson = new person("Sabbir Sun" , "23" , "Programmer");
+console.log(newPerson)

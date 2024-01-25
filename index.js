a345267708a6fd17d2015f8f9cc400cd5874b7e3
sabbirsun <sabbirsun1920@gmail.com>
2024-01-25 15:12:08 +0600

    radio btn small prohect

--- index.js
+++ index.js
@@ -262,3 +262,27 @@ loadData();
 //     .then(getServices)
 //     .then(getServicesCost)
 //     .then(console.log);*/
+
+// Radio Button
+
+const group = document.querySelector(".group");
+const output = document.querySelector(".output");
+const sizes = ["XS","S","M","L","XL","XXL"];
+
+group.innerHTML = sizes.map((size)=>`<div>
+    <input type="radio" name="size" value="${size}" id="${size}">
+    <label for="${size}">${size}</label>
+    </div>`
+).join(" ");
+
+const radioButton = document.querySelectorAll("input");
+for(const radioBtn of radioButton){
+    radioBtn.addEventListener("change", showOutput)
+}
+
+function showOutput(e){
+    console.log(e);
+    if(this.checked){
+        document.querySelector(".output").innerHTML =`You selected ${this.value}`;
+    }
+}
